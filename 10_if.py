ab45a91aa0ff7dd1f79f213cd30fc266a5583014
batouldiab <batoul96diab@gmail.com>
2025-08-07 23:34:27 +0300

    added file if

---
+++ 10_if.py
@@ -0,0 +1,26 @@
+# if
+a = 5
+b = 10
+if a>b :
+    print("a more than b")
+
+# if .. else
+a = 5
+b = 10
+if a>b :
+    print("a more than b")
+else:
+    print("b more than a")
+print("out of if")
+
+# if ...elif .. else
+a = 10
+b = 10
+if a>b :
+    print("a more than b")
+elif a<b:
+    print("b more than a")
+else:
+    print("a and b are equal")
+    
+print("out of if")
